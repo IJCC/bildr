2ce521ff6cb539857156997b4b1e534ba13aaf09
IJCC <ijcc.des@gmail.com>
2023-08-26 00:16:11 -0600

    Se subieron fotos casa bch

---
+++ casa-chg.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Somos una constructora líder en casas de diseño a la medida.">
+    <meta name="author" content="">
+    <meta property="og:site_name" content="Bildr - Despacho constructor" />
+    <meta property="og:url" content="http://www.bildr.mx" />
+    <title>Bildr - Somos una constructora líder en casas de diseño a la medida.</title>
+    <link rel="icon" type="image/png" sizes="16x16"  href="img/iconos/favicon_bildr.png">
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- Theme CSS -->
+    <link href="css/bildr.css" rel="stylesheet">
+    <link href="css/personalizado.css" rel="stylesheet">
+    <!-- WhatsApp -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet"> 
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+    <link rel="stylesheet" href="css/grid-gallery.css">
+</head>
+         
+
+<body id="page-top" class="index">
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/BILDR-logo.png" class="img-responsive center-block logotipo">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-left">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="index.html">¿Quiénes somos?</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="index.html">Servicios</a>
+                    </li>
+                    <!--<li class="page-scroll">
+                        <a href="#proyectos">Últimos proyectos</a>
+                    </li>-->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" id="desplegable" data-toggle="dropdown">Proyectos<span class="fa fa-angle-down"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="la-sexta.php">La Sexta</a></li>                
+                          <li><a href="casa-bch.php">Casa BCH</a></li>
+                          <li><a href="bar-cd.php">Bar CD</a></li>
+                          <li><a href="casa-chg.php">Casa CHG</a></li>
+                          <li><a href="casa-gj.php">Casa GJ</a></li>                
+                        </ul>
+                      </li>
+                    <li class="page-scroll">
+                        <a href="index.html">Contacto</a>
+                    </li> 
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        
+    </header>
+
+    <section class="elige-bildr">
+    
+        <div class="container">
+        <div class="row">
+        <br><br><br>
+        <h3 class="titulares-blancos">
+                    Proyecto La Sexta
+                    </h3> <br><br>
+       <center>
+       <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+<div class="row">
+  <div class="col-6 col-md-4"><img src="bildr/sexta/La_Sxta_1.png" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+  <div class="col-6 col-md-4"><img src="bildr/sexta/La_Sxta_3.png" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+  <div class="col-6 col-md-4"><img src="bildr/sexta/La_Sxta_5.png" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+</div>
+<br><br>
+<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+<div class="row">
+  <div class="col-6 col-md-4"><img src="bildr/sexta/La_Sxta_6.png" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+  <div class="col-6 col-md-4"><img src="bildr/sexta/La_Sxta_4.png" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+  <div class="col-6 col-md-4"><img src="bildr/sexta/LaSxta_Frente.jpg" class="radius-border zoom"
+             width="400px"
+             height="280px"
+             alt="Boat on Calm Water"/></div>
+</div>
+</div>
+</div>
+</section>
+    <!-- Botoón flotante WhatsApp -->
+    
+    <a href="https://api.whatsapp.com/send?phone=529995933712&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20BILDR." class="floatWapp" target="_blank">
+        <i class="fa fa-whatsapp fa-lg my-floatWapp"></i>
+        </a>
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <p class="text-footer">
+                            Calle 26 No. 353 departamento 2 x 29 y 31 Emiliano Zapata Norte, Mérida, Yucatán <br>
+                             +52 (999) 3741843
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="img/logotipo-n.png" class="img-responsive center-block logo-n">
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <ul class="ajuste-redes">
+                            <li> 
+                                <a href="">Síguenos</a> 
+                            </li>
+                            <li><a href="https://www.facebook.com/profile.php?id=100054587044190" target="_blank">
+                                <img src="img/iconos/fb.png" class="img-responsive center-block ico-redes">
+                                </a>
+                            </li>
+                            <li><a href="https://www.instagram.com/bildr_despacho_constructor/" target="_blank">
+                                <img src="img/iconos/instagram.png" class="img-responsive center-block ico-redes">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <strong class="negritas"> &copy; </strong> <strong class="negritas">Copyright</strong> <strong><span>Bildr </span></strong><strong class="negritas">Todos los derechos reservados.</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script>
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        dots:true,
+        nav:true,
+        margin:10,
+        autoplay:true,
+        autoPlaySpeed: 5000,
+        autoPlayTimeout: 5000,
+        autoPlayHoverPause: false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true
+                
+            }
+        },
+        navText : ["<i class='fa fa-arrow-left'></i>","<i class='fa fa-arrow-right'></i>"]
+    });
+      </script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+    <script src="formulario.js"></script>
+    <!-- Theme JavaScript -->
+    <script src="js/bildr.min.js"></script>
+
+</body>
+
+</html>
